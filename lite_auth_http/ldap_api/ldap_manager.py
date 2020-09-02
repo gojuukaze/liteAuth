b04d8cc7eb919f82a353e6737c7a5598f50ce983
gojuukaze <ikaze_email@163.com>
2020-09-02 20:19:05 +0800

    tree判空bug

--- lite_auth_http/ldap_api/ldap_manager.py
+++ lite_auth_http/ldap_api/ldap_manager.py
@@ -30,8 +30,8 @@ def get_k_v_from_filter_obj(f):
     else:
         raise NotImplementedError('Filter type not supported %r' % f)
     k = settings.LDAP_FIELD_MAP.get(k, k)
-    # 搜索的条件不存在，且值也不为*，则返回 (id,-1) 防止orm报错
-    if k not in UserInfo.attrs_fields() + UserInfo.other_fields() and v != '*':
+    # 搜索的条件不存在，则返回 (id,-1) 防止orm报错
+    if k not in UserInfo.attrs_fields() + UserInfo.other_fields():
         return 'id', -1
 
     if k == 'groups':
@@ -42,12 +42,12 @@ def get_k_v_from_filter_obj(f):
 
 def filter_obj_to_tree(f):
     tree = Q()
+    # 搜索条件为 k=*
     if isinstance(f, pureldap.LDAPFilter_present):
         pass
     elif isinstance(f, pureldap.LDAPFilter_equalityMatch):
         k, v = get_k_v_from_filter_obj(f)
-        if v != '*':
-            tree = Q(**{k: v})
+        tree = Q(**{k: v})
 
     elif isinstance(f, pureldap.LDAPFilter_greaterOrEqual):
         k, v = get_k_v_from_filter_obj(f)
@@ -83,6 +83,6 @@ def filter_obj_to_tree(f):
 def filter_text_to_query(filter_text):
     filter_text = filter_text.replace(' ', '')
     tree = filter_obj_to_tree(ldapfilter.parseFilter(filter_text))
-    if not tree:
+    if tree is None:
         raise ldaperrors.LDAPInappropriateMatching(filter_text)
     return UserInfo.objects.filter(tree)
