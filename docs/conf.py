a6c7a2ae7b3ccd854b24245cb9d68d30de5333c0
gojuukaze <ikaze_email@163.com>
2020-09-27 22:17:32 +0800

    文档

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ project = 'LiteAuth'
 copyright = '2020, gojuukaze'
 author = 'gojuukaze'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -48,15 +47,19 @@ language = 'zh_CN'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
+html_theme = 'my_rtd_theme'
+html_theme_path=['./']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# html_js_files = [
+#     ('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+#      {'async': 'async', 'data-ad-client': 'ca-pub-6191025081033275'})
+# ]
