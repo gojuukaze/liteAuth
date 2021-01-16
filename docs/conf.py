cd9246c31fb591dd4a70cc005065decf5dcb5361
gojuukaze <ikaze_email@163.com>
2021-01-16 21:37:58 +0800

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'my_rtd_theme'
-html_theme_path=['./']
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
--- docs/index.rst
+++ docs/index.rst
@@ -2,6 +2,7 @@
    sphinx-quickstart on Wed Sep 23 15:55:04 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
+   disable_my_rtd_theme_donate
 
 Welcome to LiteAuth
 ====================================
